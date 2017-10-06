c9f45cb91f9e01f80f9903179ad0bd57b18a40dd
Ryan Parke <ryanparke@Ryans-MacBook-Air.local>
2017-10-06 13:59:55 -0600

    dashboard and such

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className='Dashboard-page'>
+                <div className='profile-container'>
+                    <img src={logo} alt="" />
+                    <button>Edit Profile</button>
+                </div>
+                <div className='welcome-text'>
+                    <p>Welcome to Helo! Find recommended friends based on your similarities, and even search for them by name. The more you update your profile, the better recommendations we can make!</p>
+                </div>
+                <div className='friend-container'>
+                    <h2>Recommended Friends</h2>
+                    <h4>Sorted by</h4>
+                    <button>dropdown</button>
+                </div>
+
+
+
+
+            </div>
+
+        )
+    }
+}
+
+// function mapStateToProps(state) {
+//     console.log("State from private", state)
+//     return {
+//         user: state.user
+//     }
+// }
+
+// export default connect(mapStateToProps, { getUserInfo })(Private);
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+    render() {
+        return(
+            <div className='Header'>
+                <h2><b>Helo</b></h2>
+                <h3>Dashboard</h3>
+                <button>Logout</button>
+                </div>
+
+        )
+    }
+}
