687554362141b6a901be3228cce03c530f88fe80
Erin Schull <erinv.schull@Erins-MacBook-Pro.local>
2017-10-06 14:01:30 -0600

    redux stuff

---
+++ store.js
@@ -0,0 +1,7 @@
+//ERIN
+
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import reducer from './src/ducks/users';
+
+export default createStore(reducer, applyMiddleware( promiseMiddleware() ));
